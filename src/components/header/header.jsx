5ad61f3d1ad06a2bbfba4830fd1ac872d5ce33e0
1RahulP <007rahulpundir@gmail.com>
2025-01-17 15:20:17 +0530

    chnages

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -22,12 +22,22 @@ const Header = () => {
                 behavior:"smooth"
             })
         }
-        // selectedMenu.scrollIntoView({
-        //     behavior:"smooth"
-        // })
+        
     }
+    const [background, setBackground] = React.useState("bg-transparent")
+    // const header = document.getElementById('header');
+
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        
+        setBackground("bg-white border-b")
+      } else {
+        setBackground("bg-transparent")
+        
+      }
+    });
     return (
-        <div className="bg-[#ffffffde] fixed top-0 z-[9] w-full border-b sm:py-4 px-4 py-2">
+        <div className={`fixed top-0 z-[9] w-full sm:py-4 px-4 py-2 ${background}`}>
             <div className="max-w-[1250px] mx-auto flex justify-between items-center">
                 <div onClick={()=>scrollToNavigate("Home")}>
                     <img src="/insurance.png" alt="logo" width={54} />
@@ -36,44 +46,19 @@ const Header = () => {
                     {menuArray?.map((item,index)=>{
                         return(
 
-                            <p className="cursor-pointer" onClick={()=>scrollToNavigate(item?.menu)}>{item?.menu}</p>
+                            <p key={index} className="cursor-pointer" onClick={()=>scrollToNavigate(item?.menu)}>{item?.menu}</p>
                         )
                     })}
                     <p className="cursor-pointer" >Raise A Claim</p>
                 </div>
-                <div className="sm:block hidden" onClick={()=>scrollToNavigate("contact")}>
+                <div onClick={()=>scrollToNavigate("contact")}>
                     <button className="border px-4 py-2 rounded-md bg-white bg-gradient-to-r from-blue-200 via-purple-200 to-pink-200">Become A Partner</button>
                 </div>
-                <div className="sm:hidden block cursor-pointer" onClick={() => setToggleMenu(true)}>
+                {/* <div className="sm:hidden block cursor-pointer" onClick={() => setToggleMenu(true)}>
                     <img src="/png/menu.png" alt="menu" width={24} />
-                </div>
+                </div> */}
             </div>
-            {toggleMenu &&
-                <div className="bg-white text-white fixed top-0 h-screen w-screen left-0 py-2 px-4">
-                    <div className="flex items-center justify-between">
-                        <img src="/insurance.png" alt="logo" width={54} />
-                    <img src="/png/close.png" alt="menu" width={24} className="flex-none" onClick={()=>setToggleMenu(false)} />
-
-                    </div>
-                    <div className="h-[91%] grid">
-                        <div className="text-black text-lg grid gap-3 h-fit mt-8">
-                            <p className="cursor-pointer bg-gradient-to-r from-blue-100 via-blue-100  border-blue-800 to-blue-50 rounded-md px-3 py-1.5 h-fit">Car</p>
-                            <p className="cursor-pointer bg-gradient-to-r from-blue-100 via-blue-100  border-blue-800 to-blue-50 rounded-md px-3 py-1.5 h-fit">Bike</p>
-                            <p className="cursor-pointer bg-gradient-to-r from-blue-100 via-blue-100  border-blue-800 to-blue-50 rounded-md px-3 py-1.5 h-fit">Health</p>
-                            <p className="cursor-pointer bg-gradient-to-r from-blue-100 via-blue-100  border-blue-800 to-blue-50 rounded-md px-3 py-1.5 h-fit">Life</p>
-                        </div>
-                        <div className="my-auto ">
-                            <p className="text-blue-700 font-semibold mb-2">
-                            Raise A Claim
-                            </p>
-                            <p className="text-blue-700 font-semibold">
-                                Become A Partner
-                            </p>
-
-                        </div>
-                    </div>
-                </div>
-            }
+            
         </div>
     )
 }
